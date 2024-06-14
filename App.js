769b4c49c1fd6ba498fa405836fbb93af19a5e82
ifasapp-admin <ashish.j@ifasonline.com>
2024-06-15 05:08:45 +0530

    font linking

--- App.js
+++ App.js
@@ -3,8 +3,18 @@ import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import RootNavigator from './src/routes/RootNavigator';
 import {store, persistor} from './src/redux/store';
+import { useFonts } from 'expo-font';
+
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins_Regular: require('./assets/fonts/Poppins-Regular.ttf'),// 400
+    Poppins_Medium: require('./assets/fonts/Poppins-Medium.ttf'), // 500
+    Poppins_Bold: require('./assets/fonts/Poppins-Bold.ttf'),     // 700
+    Poppins_Italic: require('./assets/fonts/Poppins-Italic.ttf'),  // 400
+    Poppins_MediumItalic: require('./assets/fonts/Poppins-MediumItalic.ttf'), // 500
+    Poppins_BoldItalic: require('./assets/fonts/Poppins-BoldItalic.ttf'), // 700
+  });
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
